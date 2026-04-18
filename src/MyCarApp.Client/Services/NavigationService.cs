07a6b37f851b7d4b6536cd2e2e54e485719a67c8
Marcos <marcoslaranz@gmail.com>
2026-04-19 09:24:59 +1200

    Fix navigation for GitHub Pages base path

---
+++ src/MyCarApp.Client/Services/NavigationService.cs
@@ -0,0 +1,18 @@
+namespace MyCarApp.Client.Services;
+
+public class NavigationService
+{
+    private readonly NavigationManager _nav;
+
+    public NavigationService(NavigationManager nav)
+    {
+        _nav = nav;
+    }
+
+    public void NavigateTo(string path)
+    {
+        // Remove leading slash for relative navigation
+        var cleanPath = path.TrimStart('/');
+        _nav.NavigateTo(_nav.BaseUri + cleanPath);
+    }
+}
