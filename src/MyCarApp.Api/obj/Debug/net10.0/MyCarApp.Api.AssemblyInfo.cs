684756df12955c6c873284202dcbed69cf0b19c0
Marcos <marcoslaranz@gmail.com>
2026-05-03 17:59:23 +1200

    Add CSV and Excel export feature

--- src/MyCarApp.Api/obj/Debug/net10.0/MyCarApp.Api.AssemblyInfo.cs
+++ src/MyCarApp.Api/obj/Debug/net10.0/MyCarApp.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCarApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807646b8dd7aa0ceeaba8abd6a324d80dc097845")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c0a01518e0270450e71608dd2db4410a02ada3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCarApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCarApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
